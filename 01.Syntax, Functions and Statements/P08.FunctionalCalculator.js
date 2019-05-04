89db29c90b3c192c331cc4211e836de203445a28
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-04 23:25:06 +0300

    initial commit

---
+++ 01.Syntax, Functions and Statements/P08.FunctionalCalculator.js
@@ -0,0 +1,13 @@
+function solve(firstNumber, secondNumber, operator){
+    let result;
+
+    switch (operator) {
+        case '+': result = firstNumber + secondNumber; break;
+        case '-': result = firstNumber - secondNumber; break;
+        case '*': result = firstNumber * secondNumber; break;
+        case '/': result = firstNumber / secondNumber; break;
+        case '%': result = firstNumber % secondNumber; break;
+        case '**': result = firstNumber ** secondNumber; break;
+    }
+    console.log(result);
+}
