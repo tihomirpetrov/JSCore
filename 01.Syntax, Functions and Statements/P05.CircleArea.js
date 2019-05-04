ba531ba19465b1d6dda4d3e5275036a7990fe5ad
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-04 22:08:42 +0300

    initial commit

---
+++ 01.Syntax, Functions and Statements/P05.CircleArea.js
@@ -0,0 +1,13 @@
+function solve(input) {
+    let inputType = typeof(input);
+
+    if (inputType === 'number')
+    {
+        let area = Math.pow(input,2) * Math.PI;
+        console.log(area.toFixed(2));
+    }
+    else
+        {
+            console.log(`We can not calculate the circle area, because we receive a ${inputType}.`);
+        }
+}
