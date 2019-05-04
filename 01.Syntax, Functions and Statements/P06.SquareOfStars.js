6da62e6c9098014ce3aac3e9a005894186ade541
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-04 22:54:25 +0300

    initial commit

---
+++ 01.Syntax, Functions and Statements/P06.SquareOfStars.js
@@ -0,0 +1,13 @@
+function solve(num) {
+
+       for (let i = 1; i <= num; i++)
+       {
+           for (let j = 1; j <= num ; j++)
+           {
+              process.stdout.write('*');
+              process.stdout.write(' ');
+           }
+           console.log();
+       }
+}
+solve(2);
