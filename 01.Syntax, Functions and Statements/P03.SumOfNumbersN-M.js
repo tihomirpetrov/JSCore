d3ee0bdf12ede7b113041f0fce8da747e7272d73
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-04 19:28:37 +0300

    initial commit

---
+++ 01.Syntax, Functions and Statements/P03.SumOfNumbersN-M.js
@@ -0,0 +1,12 @@
+function solve(n, m) {
+    let result = 0;
+
+    let firstNumber = Number(n);
+    let secondNumber = Number(m);
+
+    for (let i = firstNumber; i <= secondNumber ; i++) {
+        result += i;
+    }
+
+    return result;
+}
