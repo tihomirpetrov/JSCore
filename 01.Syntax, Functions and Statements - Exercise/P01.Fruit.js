a47949bfa47dc320f75d0e1e3efb9d069bd1ca4d
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-05 16:26:44 +0300

    initial commit

--- 01.Syntax, Functions and Statements - Exercise/P01.Fruit.js
+++ 01.Syntax, Functions and Statements - Exercise/P01.Fruit.js
@@ -1,3 +1,8 @@
-function solve() {
-    
+function solve(string, weight, price) {
+    let fruit = string;
+    let fruitWeight = weight / 1000;
+    let fruitPrice = price * fruitWeight;
+    let money = 0;
+
+    console.log(`I need $${fruitPrice.toFixed(2)} to buy ${fruitWeight.toFixed(2)} kilograms ${fruit}.`);
 }
