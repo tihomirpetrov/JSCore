af7296ec9d67c9b417af42a255865c1ff8707e50
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-05 12:05:33 +0300

    initial commit

--- 01.Syntax, Functions and Statements/P09.AggregateElements.js
+++ 01.Syntax, Functions and Statements/P09.AggregateElements.js
@@ -1,14 +1,15 @@
 function solve(array) {
 
     let sumElements = 0;
+    let sumInverseElements = 0;
     let concatElements;
 
     for (let i = 0, l = array.length; i < l; i++) {
         sumElements += array[i];
+        sumInverseElements += 1/array[i];
     }
 
     console.log(sumElements);
+    console.log(sumInverseElements);
     console.log(array.join(''));
 }
-
-solve([1, 2, 3]);
