24c04a37e0919a3b9794851055f633fc7563058c
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-10 17:40:44 +0300

    initial commit

--- 01.Syntax, Functions and Statements - Exercise/P04.TimeToWalk.js
+++ 01.Syntax, Functions and Statements - Exercise/P04.TimeToWalk.js
@@ -1,12 +1,12 @@
 function solve(steps, length, speed) {
     let numberOfSteps = steps;
-    let lengthInKm = (length  * steps) / 1000;
+    let lengthInKm = (length * steps) / 1000;
     let studentSpeed = speed;
     let time = (lengthInKm / studentSpeed);
     let rest = lengthInKm / 0.5;
     let hours = 0;
     let minutes = Math.floor(time * 60);
-    let seconds = Math.round(((time * 60) - minutes) * 60) ;
+    let seconds = Math.round(((time * 60) - minutes) * 60);
     minutes = Math.floor(minutes + rest);
-    console.log(`${hours.toString().padStart(2,0)}:${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2,0)}`);
+    console.log(`${hours.toString().padStart(2, 0)}:${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2, 0)}`);
 }
