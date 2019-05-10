83c4d983f839b722367137ae4c1a9e60c3fc2532
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-10 19:39:43 +0300

    initial commit

---
+++ 01.Syntax, Functions and Statements - Exercise/P05.CalorieObject.js
@@ -0,0 +1,17 @@
+function solve(arr) {
+
+    let name = arr[0];
+    let calories = arr[1];
+    let caloriesObj = "";
+
+    process.stdout.write("{ ");
+    for (let i = 0; i < arr.length - 2; i++) {
+        if (i % 2 == 0) {
+            process.stdout.write(`${arr[i]}: ${arr[i + 1]}, `);
+        }
+    }
+    process.stdout.write(`${arr[arr.length - 2]}: ${arr[arr.length - 1]}`);
+    process.stdout.write(" }");
+}
+
+solve(['Yoghurt', 48, 'Rise', 138, 'Apple', 52]);
