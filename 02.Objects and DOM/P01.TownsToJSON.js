b1734505b8138d82cf99db8c58cde4efa76a2db2
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-11 21:26:18 +0300

    initial commit

---
+++ 02.Objects and DOM/P01.TownsToJSON.js
@@ -0,0 +1,19 @@
+function solve(towns) {
+
+    let townsArr = [];
+    for (let town of towns.slice(1)) {
+        let [empty,townName,lat,lng]=town.split(/\s*\|\s*/);
+        let townObj = {
+            Town: townName,
+            Latitude: Number(lat),
+            Longitude: Number(lng)
+        };
+        townsArr.push(townObj);
+    }
+    console.log(JSON.stringify(townsArr));
+}
+
+solve(['| Town | Latitude | Longitude |',
+    '| Veliko Turnovo | 43.0757 | 25.6172 |',
+    '| Monatevideo | 34.50 | 56.11 |']
+);
