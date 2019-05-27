37a4a211f978676e56edaffd5adc7aec2295e8b0
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-28 00:39:27 +0300

    initial commit

--- 02.Objects and DOM/P05.ArticlesList/solution.js
+++ 02.Objects and DOM/P05.ArticlesList/solution.js
@@ -1,4 +1,26 @@
 function createArticle() {
-	
-	//TODO...
+
+    let createTitleElement = document.getElementById('createTitle');
+    let createTitleValue = createTitleElement.value;
+
+    let createTextElement = document.getElementById('createContent');
+    let createTextValue = createTextElement.value;
+
+    if (createTitleValue && createTextValue) {
+        let titleElement = document.createElement('h3');
+        titleElement.textContent = createTitleValue;
+
+        let textElement = document.createElement('p');
+        textElement.textContent = createTextValue;
+
+        let articleElement = document.createElement('article');
+        articleElement.appendChild(titleElement);
+        articleElement.appendChild(textElement);
+
+        let articlesElement = document.getElementById('articles');
+        articlesElement.appendChild(articleElement);
+
+        createTitleElement.value = '';
+        createTextElement.value = '';
+    }
 }
