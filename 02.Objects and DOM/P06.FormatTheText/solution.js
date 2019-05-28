e48a9bc57101796e1d9d16f648d0b5e7ea451fb4
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-28 03:06:42 +0300

    initial commit

--- 02.Objects and DOM/P06.FormatTheText/solution.js
+++ 02.Objects and DOM/P06.FormatTheText/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO...
+    let paragraphToChange = document.getElementById('input');
+    let paragraphContent = paragraphToChange.textContent;
+
+    let allParagraphSentences = paragraphContent.toString().split('. ');
+    let outputDiv = document.getElementById('output');
+
+    for (let i = 0; i < allParagraphSentences.length; i += 3) {
+        let createParagraphElement = document.createElement('p');
+        createParagraphElement.textContent = allParagraphSentences[i] + '.';
+
+        if (allParagraphSentences[i + 1] != null) {
+            createParagraphElement.textContent += ' ' + allParagraphSentences[i + 1] + '.';
+        }
+        if (allParagraphSentences[i + 2] != null) {
+            createParagraphElement.textContent += ' ' + allParagraphSentences[i + 2] + '.';
+        }
+
+        outputDiv.appendChild(createParagraphElement);
+    }
 }
