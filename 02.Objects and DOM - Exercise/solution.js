8e2f068f93b51537c2550564806bf0eeba595a7b
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-31 00:25:09 +0300

    initial commit

---
+++ 02.Objects and DOM - Exercise/solution.js
@@ -0,0 +1,19 @@
+function solve() {
+
+    let targetDiv = document.getElementsByClassName("my-message")[0];
+    let sendBtn = document.getElementById("send");
+    let chatInput = document.getElementById("chat_input");
+    let chatMsgArea = document.getElementById("chat_messages");
+
+    sendBtn.addEventListener("click", function () {
+        let targetDivClone = targetDiv.cloneNode(true);
+        let chatInputText = chatInput.value;
+
+        if (chatInputText.length > 0) {
+            targetDivClone.textContent = chatInputText;
+            chatMsgArea.appendChild(targetDivClone);
+
+            chatInput.value = "";
+        }
+    });
+}
