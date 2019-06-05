4222d7d5b2f619b37554e4f9ebd404f6989fcad2
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-06-05 16:42:54 +0300

    initial commit

--- 03.Arrays and Matrices/02.EvenPositionElement.js
+++ 03.Arrays and Matrices/02.EvenPositionElement.js
@@ -2,10 +2,11 @@ function solve(arr) {
 
     let evenElements = [];
 
+
     for (let i = 0; i < arr.length; i++) {
-        
-        if (i % 2 == 0){
-            evenElements[i] = Number(arr[i]);
+
+        if (i % 2 == 0) {
+            evenElements.push(arr[i]);
         }
     }
     console.log(evenElements.join(" "));
