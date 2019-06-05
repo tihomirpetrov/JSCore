7a44225b3fdb289aa101122e4adfe4a480e289f2
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-06-05 14:26:35 +0300

    initial commit

---
+++ 03.Arrays and Matrices/01.SumFirstLast.js
@@ -0,0 +1,10 @@
+function solve(arr) {
+
+    let firstNumber = Number(arr[0]);
+    let lastNumber = Number(arr[arr.length - 1]);
+
+    let sum = firstNumber + lastNumber;
+
+    console.log(sum);
+}
+solve(['20', '30', '40']);
