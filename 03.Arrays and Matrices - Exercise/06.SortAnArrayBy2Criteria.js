25679f8b00709a353d7af3ec53cd85b0c3cb5c9e
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-06-07 13:38:11 +0300

    initial commit

--- 03.Arrays and Matrices - Exercise/06.SortAnArrayBy2Criteria.js
+++ 03.Arrays and Matrices - Exercise/06.SortAnArrayBy2Criteria.js
@@ -1,9 +1,23 @@
-function solve() {
-    
+function solve(arr) {
+
+    arr.sort(function(a, b){
+        let nameA=a.toLowerCase(), nameB=b.toLowerCase();
+        if (nameA < nameB)
+            return -1;
+        if (nameA > nameB)
+            return 1;
+        return 0;
+    });
+    arr.sort((a, b) => a.length - b.length);
+
+    console.log(arr.join("\n"));
 }
 
 
-solve(['alpha',
-    'beta',
-    'gamma']
+solve(['Isacc',
+    'Theodor',
+    'Jack',
+    'Harrison',
+    'George']
+
 );
