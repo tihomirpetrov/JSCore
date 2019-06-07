92eb0507acba8c3892619a5fe7cf198847409902
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-06-07 20:59:30 +0300

    initial commit

---
+++ 03.Arrays and Matrices - Exercise/08.SpiralMatrix.js
@@ -0,0 +1,42 @@
+function matrix(row, col) {
+    let result = new Array(row).fill().map(() => new Array(row).fill('')); // create empty n x n array
+    let counter = 1;
+    let startCol = 0;
+    let endCol = row - 1;
+    let startRow = 0;
+    let endRow = row - 1;
+    while (startCol <= endCol && startRow <= endRow) {
+        for (let i = startCol; i <= endCol; i++) {
+            result[startRow][i] = counter;
+            counter++;
+        }
+        startRow++;
+        for (let j = startRow; j <= endRow; j++) {
+            result[j][endCol] = counter;
+            counter++;
+        }
+
+        endCol--;
+
+        for (let i = endCol; i >= startCol; i--) {
+            result[endRow][i] = counter;
+            counter++;
+        }
+
+        endRow--;
+        for (let i = endRow; i >= startRow; i--) {
+            result[i][startCol] = counter;
+            counter++;
+        }
+
+        startCol++;
+    }
+
+    for (let row of result) {
+
+        console.log(row.join(' '));
+
+    }
+}
+
+matrix(3, 3);
