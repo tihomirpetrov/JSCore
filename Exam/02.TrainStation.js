f718cc3324365059df028f3f19acc3b51cb39295
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-06-09 09:51:16 +0300

    initial commit

--- Exam/02.TrainStation.js
+++ Exam/02.TrainStation.js
@@ -1,5 +1,27 @@
-function solve() {
+function solve(capacity, arr) {
+
+    let capacityOfWagon = Number(capacity);
+    let passengersForEachWagon = arr;
+    let wagons = arr.length;
+    let passengersAboard = [];
+
+    for (let i = 0; i < wagons; i++) {
+
+        let passengersForNextWagon = 0;
+        if (passengersForEachWagon[i] <= capacityOfWagon) {
+
+            passengersAboard.push(passengersForEachWagon[i]);
+            continue;
+        } else if (passengersForEachWagon[i] > capacityOfWagon) {
+
+            passengersForNextWagon = passengersForEachWagon[i] - capacityOfWagon;
+
+            passengersAboard.push(capacityOfWagon);
+        }
+
+    }
+
 
 }
 
-solve();
+solve(10, [9, 39, 1, 0, 0]);
