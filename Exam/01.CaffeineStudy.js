4a29951d85af56690b7eaa8d71b3aa2f141adfc5
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-06-09 10:07:03 +0300

    initial commit

--- Exam/01.CaffeineStudy.js
+++ Exam/01.CaffeineStudy.js
@@ -1,34 +1,34 @@
 function solve(days) {
 
     let durationInDays = Number(days);
-    let coffee = Number(40);
-    let cocacola = Number(8);
-    let tea = Number(20);
-    let energy = Number(30);
-
-    let morning = Number(3);
-    let lunch = Number(2);
-    let brunch = Number(3);
+    // let coffee = Number(40);
+    // let cocacola = Number(8);
+    // let tea = Number(20);
+    // let energy = Number(30);
+    //
+    // let morning = Number(3);
+    // let lunch = Number(2);
+    // let brunch = Number(3);
 
     let caffeine = Number(0);
 
+    if(durationInDays >=1 && durationInDays <= 30){
+        for (let i = 1; i <= durationInDays ; i++) {
 
-    for (let i = 1; i <= durationInDays ; i++) {
+            caffeine += 180; //morning 3 cups of coffee
+            caffeine += 40; //every lunch 2 bottles of coca cola
+            caffeine += 210; //every brunch 3 cups of tea
 
-        caffeine += 180; //morning 3 cups of coffee
-        caffeine += 40; //every lunch 2 bottles of coca cola
-        caffeine += 210; //every brunch 3 cups of tea
-        
-        if (i % 5 == 0){
-            caffeine += 450; //3 energy drinks every 5th day
+            if (i % 5 == 0){
+                caffeine += 450; //3 energy drinks every 5th day
+            }
+            if(i % 9 == 0){
+                caffeine += 300; //+2 bottles of coca cola and 2 energy drinks * 100ml
+            }
         }
-        if(i % 9 == 0){
-            caffeine += 100; //+2 bottles of coca cola and 2 energy drinks * 100ml
-        }
-
     }
 
     console.log(`${caffeine} milligrams of caffeine were consumed`);
 }
 
-solve(8);
+solve(9);
--- Exam/02.TrainStation.js
+++ Exam/02.TrainStation.js
@@ -4,23 +4,28 @@ function solve(capacity, arr) {
     let passengersForEachWagon = arr;
     let wagons = arr.length;
     let passengersAboard = [];
+    let passengersForNextWagon = 0;
 
     for (let i = 0; i < wagons; i++) {
 
-        let passengersForNextWagon = 0;
+
         if (passengersForEachWagon[i] <= capacityOfWagon) {
 
             passengersAboard.push(passengersForEachWagon[i]);
             continue;
         } else if (passengersForEachWagon[i] > capacityOfWagon) {
 
-            passengersForNextWagon = passengersForEachWagon[i] - capacityOfWagon;
+            passengersForEachWagon[i] -= capacityOfWagon;
+            let passengersIn = capacityOfWagon;
+            passengersAboard.push(passengersIn);
 
-            passengersAboard.push(capacityOfWagon);
+            passengersForNextWagon += passengersForEachWagon[i] - capacityOfWagon;
+
         }
 
     }
 
+    console.log(passengersAboard);
 
 }
 
